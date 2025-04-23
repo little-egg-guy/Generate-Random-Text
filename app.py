57e3d5afb13459b108712a023cda6887d5642c0c
little-egg-guy <waveb272@gmail.com>
2025-04-23 18:14:15 +0800

    DO IT

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def generate_text():
 # Home route
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return send_from_directory('.', 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
